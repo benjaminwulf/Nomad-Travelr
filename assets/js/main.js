43c1e75da9c332555faf7a20b200667a247fe553
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-07-07 16:51:54 -0600

    got foursquare ajax call in test working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,10 @@
-/*add this to your html -> <script src="https://storage.googleapis.com/code-snippets/rapidapi.min.js"></script> */
-var rapid = new RapidAPI("default-application_5b29a84fe4b08122a9a22912", "77a8df6e-81f2-4f4e-b9e3-fd31fbd2e8bd");
+/**********FourSquare***************/
+var ll = '45.5589522,-122.6517163'
+var queryURL = 'https://api.foursquare.com/v2/venues/search?ll=' + ll + '&limit=10&client_id=ITN3RTSLWS0EZ1NZ0NKWQBNJBUSE2F44N43VS5ZI0BYN0EHA&client_secret=1GDMIQM0YKKKWYAQH0WVTAYBLIJ3YXZJMDIAOHDVFXFJI4DC&v=20140806'
 
-rapid.call('YelpAPI', 'getAutocomplete', { 
-	'accessToken': 'pi1sE9K0i_sVdhYgLAKtZdlcG6z5H3Sxeyhs-a-MXO055d68GMzmV8MRoCQjj_zqdIUIhlAX1zMJ3BK5Wn-n0iZe4poD9xW2QJGN3E1UfuXCd0YITiAk1DIczOhAW3Yx',
-	'coordinate': '39.6951107, -104.98878309999999',
-	'text': 'record shop'
-
-}).on('success', function (payload) {
-	 /*YOUR CODE GOES HERE*/ 
-}).on('error', function (payload) {
-	 /*YOUR CODE GOES HERE*/ 
-});
-
-
-$(document).on('click', "#search-button", function () {
-
-var querySearch = "denver"
-
-var queryURL = "https://api.yelp.com/v3/autocomplete" +
-
-}
+ $.ajax({
+    url: queryURL,
+    method: 'GET'
+ }).then (function (data) {
+        console.log(data.response);
+ });
