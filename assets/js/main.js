7985acba6a4e728f85365aa9fab2b1b96911bd1e
ezemp <ericzemp@gmail.com>
2018-07-11 21:59:42 -0600

    actually added two methods to sign in with google

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,3 +204,38 @@ firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
     });
   
   })
+  // ====Sign In With Google Two Methods========
+  // =====Method One Pop Up=====
+  $("#gmailSignIn").on("click",function googleSignIn(){
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("Error : " + errorMessage);
+      })})
+    
+    // }
+    // $("#gmailSignIn").on("click",function googleSignIn(){{
+    //   var provider = new firebase.auth.GoogleAuthProvider();
+    //   firebase.auth().getRedirectResult().then(function(result) {
+    //   if (result.credential) {
+    //     // This gives you a Google Access Token. You can use it to access the Google API.
+    //     var token = result.credential.accessToken;
+    //     // ...
+    //   }
+    //   // The signed-in user info.
+    //   var user = result.user;
+    // }).catch(function(error) {
+    //   // Handle Errors here.
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   var email = error.email;
+    //   // The firebase.auth.AuthCredential type that was used.
+    //   var credential = error.credential;
+    //   // ...
+    // })};
