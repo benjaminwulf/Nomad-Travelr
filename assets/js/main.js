21fcc80d91d657d913ff8c9d890252c44accae36
ezemp <ericzemp@gmail.com>
2018-07-11 21:47:05 -0600

    added to potential choices for signing in with google

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,3 +204,43 @@ firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
     });
   
   })
+  // ==========Sign in With Gmail account 2 Methods=========
+  // ======Required for both===========================
+  var provider = new firebase.auth.GoogleAuthProvider();
+  // ==================================================
+  // =====Method One Page Redirect Untested So Far Can't Say It Works========
+
+
+  // $("#gmailSignIn").on("click",function gmailsignIn(){
+  //   firebase.auth().getRedirectResult().then(function(result) {
+  //   if (result.credential) {
+  //     // This gives you a Google Access Token. You can use it to access the Google API.
+  //     var token = result.credential.accessToken;
+  //     // ...
+  //   }
+  //   // The signed-in user info.
+  //   var user = result.user;
+  // }).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   var email = error.email;
+  //   // The firebase.auth.AuthCredential type that was used.
+  //   var credential = error.credential;
+  //   // ...
+  // })});
+
+// ========Method Two Pop Up============
+$("#gmailSignIn").on("click",function googleSignIn(){
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    
+       console.log("Error : " + errorMessage);
+      })
+})
