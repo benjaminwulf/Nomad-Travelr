62134b60857194359befaf6c63a0ed85281d4df3
Olga <oserenchenko@gmail.com>
2018-07-12 21:21:22 -0600

    added firebase auth - Eric

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ var database = firebase.database();
 var map, infoWindow;
 var cityName = [];
 // Global variable for searching with Foursquare API
-var nearVenue = "";
+var cityName = "";
 
 //INTIAL MAP FUNCTION
 function initMap() {
@@ -34,7 +34,7 @@ function initMap() {
     var lat = childSnapshot.val().lat;
     var long = childSnapshot.val().long;
     var newCityDiv = $('<div>');
-    newCityDiv.addClass(cityName);
+    newCityDiv.addClass(cityName.split(",")[0]);
     newCityDiv.append('<button class="favCityButton" value="' + cityName + '">' + cityName + '</button>')
     $('.favCities').append(newCityDiv);
     var marker = new google.maps.Marker({
@@ -44,6 +44,11 @@ function initMap() {
         lng: long
       }
     })
+  });
+  database.ref('venues').on("child_added", function (childSnapshot) {
+    var newAddedVenue = childSnapshot.val().venueName;
+    var cityToAddTo = childSnapshot.val().inCity.split(",")[0];
+    $('.' + cityToAddTo).append('<p class="favVenue">' + newAddedVenue + '</p>');
   });
 }
 
@@ -84,12 +89,12 @@ $(document).on('click', '.favCityButton', function () {
   // This global variable is to be passed Foursquare AJAX call
   $('.city-form').empty();
   $('.city-venues').empty();
-  nearVenue = $(this).val();
+  cityName = $(this).val();
   newForm = $('<form>');
   newForm.append('<input class="formInput" type="text" value="What would you like to do here?">');
   newForm.append('<button class="formSubmit">Submit</button>')
   $('.city-form').append(newForm);
-  return nearVenue;
+  return cityName;
 });
 
 $(document).on('click', '.formSubmit', function (event) {
@@ -97,7 +102,7 @@ $(document).on('click', '.formSubmit', function (event) {
   $('.city-venues').empty();
   var searchTerm = $('.formInput').val().trim();
   // AJAX call for venue name limit 10
-  var queryURL = 'https://api.foursquare.com/v2/venues/search?near=' + nearVenue + '&query=' + searchTerm + '&limit=10&client_id=ITN3RTSLWS0EZ1NZ0NKWQBNJBUSE2F44N43VS5ZI0BYN0EHA&client_secret=1GDMIQM0YKKKWYAQH0WVTAYBLIJ3YXZJMDIAOHDVFXFJI4DC&v=20140806'
+  var queryURL = 'https://api.foursquare.com/v2/venues/search?near=' + cityName + '&query=' + searchTerm + '&limit=10&client_id=ITN3RTSLWS0EZ1NZ0NKWQBNJBUSE2F44N43VS5ZI0BYN0EHA&client_secret=1GDMIQM0YKKKWYAQH0WVTAYBLIJ3YXZJMDIAOHDVFXFJI4DC&v=20140806'
   $.ajax({
     url: queryURL,
     method: 'GET'
@@ -118,14 +123,14 @@ $(document).on('click', '.formSubmit', function (event) {
       });
       var newAddButton = $('<button class="addButton">+Add</button>');
       newAddButton.attr({
-        cityName: nearVenue,
+        cityName: cityName,
         venueName: queryName,
         latitude: lat,
         longitude: lng
       });
       var newUpVoteButton = $('<button class="upVoteButton"><i class="fas fa-thumbs-up"></i></button>');
       newUpVoteButton.attr({
-        cityName: nearVenue,
+        cityName: cityName,
         venueName: queryName,
         latitude: lat,
         longitude: lng
@@ -143,6 +148,7 @@ $(document).on('click', '.addButton', function (event) {
   var addedVenueName = $(this).attr("venueName");
   var addedVenueLat = $(this).attr("latitude");
   var addedVenueLng = $(this).attr("longitude");
+  console.log(addedVenueName);
   database.ref('venues').push({
     inCity: addedCity,
     venueName: addedVenueName,
@@ -152,10 +158,92 @@ $(document).on('click', '.addButton', function (event) {
 });
 
 
-  database.ref('venues').on("child_added", function (childSnapshot) {
-    var newAddedVenue = childSnapshot.val().venueName;
-    var cityToAddTo = childSnapshot.val().inCity;
-    console.log('.' + cityToAddTo);
-    $('.Tokyo, Japan').append('hello');
+// =========Fire base auth code==========
+// This allows us to change what is displayed on the screen
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // Display Map If User is signed in.
+    document.getElementById("loggedIn-view").style.display = "block";
+    document.getElementById("login-view").style.display = "none";
+    var user = firebase.auth().currentUser;
+    // If we have a user we can use this to Display their name on the web page
+    if (user != null) {
+      var email_id = user.Email;
+    }
+  } else {
+    // Display Log-in Page if user isnt signed in
+    document.getElementById("loggedIn-view").style.display = "none";
+    document.getElementById("login-view").style.display = "block";
+  }
+});
+// =====RegisterUser=====
+$("#register").on("click", function register() {
+  userEmail = document.getElementById("email-input").value;
+  var userPass = document.getElementById("pass-input").value;
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+    .catch(function (error) {
+      console.log("Error : " + errorMessage);
+    }).then(
+      function writeUserData() {
+        firebase.database().ref('users/' + userEmail.replace(".", "")).set({
+          email: userEmail
+        });
+      })
+})
+
+
+// ======LogOut Function====
+$("#logOut").on("click", function logout() {
+  firebase.auth().signOut();
+})
+// =====LogIn Function=====
+
+$("#logIn").on("click", function login() {
+  var userEmail = document.getElementById("email-input").value;
+  var userPass = document.getElementById("pass-input").value;
+  console.log(userEmail);
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error : " + errorMessage);
+  })
+})
+// ====Sign In With Google Two Methods========
+// =====Required for both=====
+var provider = new firebase.auth.GoogleAuthProvider();
+// =====Method One Pop Up=====
+$("#gmailSignIn").on("click", function googleSignIn() {
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error : " + errorMessage);
   });
+})
+
+// ==========Method Two Page Redirect Haven't Tested Can't say for sure it works=======
+// 
+// $("#gmailSignIn").on("click",function googleSignIn(){{
+//   firebase.auth().getRedirectResult().then(function(result) {
+//   if (result.credential) {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     var token = result.credential.accessToken;
+//     // ...
+//   }
+//   // The signed-in user info.
+//   var user = result.user;
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// })}});
+// 
 
